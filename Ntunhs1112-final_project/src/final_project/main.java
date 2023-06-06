4259c858e2b76d7f62e632ea1f71d99c5e1a06d4
wilson930626 <wilson20040626@gmail.com>
2023-06-06 19:35:20 +0800

    增加了一些書

--- Ntunhs1112-final_project/src/final_project/main.java
+++ Ntunhs1112-final_project/src/final_project/main.java
@@ -4,10 +4,11 @@ public class main {
 
 	public static void main(String[] args) {
 		Book b1 = new Book("現代統計學","林惠玲、陳正倉","978-957-9096-80-5",2020,7);
-		Book b2 = new Book("統計學","林惠玲、陳正倉","978-957-9096-80-5",2020,7);
-		Book b3 = new Book("現代統計學","林惠玲、陳正倉","978-957-9096-80-5",2020,7);
-		Book b4 = new Book("現代統計學","林惠玲、陳正倉","978-957-9096-80-5",2020,7);
-		Book b5 = new Book("現代統計學","林惠玲、陳正倉","978-957-9096-80-5",2020,7);
+		Book b2 = new Book("基礎微積分","張三奇、伍志祥、王永成、張力友","978-986-5647-85-8",2017,3);
+		Book b3 = new Book("管理學","方至民、曾志弘","978-986-98227-2-5",2020,5);
+		Book b4 = new Book("Bootstrap5RWD","陳惠貞","978-986-312-659-1",2022,2);
+		Book b5 = new Book("C++物件導向","高橋麻奈","978-986-201-699-2",2020,7);
+		Book b6 = new Book("English Now! Listening and Speaking in Everyday Life 3","高橋麻奈","978-626-300-055-1",2020,7);
 	}
 
 }
