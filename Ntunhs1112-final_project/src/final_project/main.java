924538a1ab65c2e56554093c6c3aa31ace56f813
wilson930626 <wilson20040626@gmail.com>
2023-06-06 22:11:10 +0800

    加了MIS

--- Ntunhs1112-final_project/src/final_project/main.java
+++ Ntunhs1112-final_project/src/final_project/main.java
@@ -9,6 +9,7 @@ public class main {
 		Book b4 = new Book("Bootstrap5RWD","陳惠貞","978-986-312-659-1",2022,2);
 		Book b5 = new Book("C++物件導向","高橋麻奈","978-986-201-699-2",2020,7);
 		Book b6 = new Book("English Now! Listening and Speaking in Everyday Life 3","高橋麻奈","978-626-300-055-1",2020,7);
+		Book b7 = new Book("資訊管理系統","洪錦魁","978-986-7696-39-7",2021,10);
 	}
 
 }
