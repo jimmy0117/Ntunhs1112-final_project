4822d025bded794d237ad16e09df203f03b052b3
wilson930626 <wilson20040626@gmail.com>
2023-06-06 22:15:13 +0800

    加了java

--- Ntunhs1112-final_project/src/final_project/main.java
+++ Ntunhs1112-final_project/src/final_project/main.java
@@ -9,7 +9,8 @@ public class main {
 		Book b4 = new Book("Bootstrap5RWD","陳惠貞","978-986-312-659-1",2022,2);
 		Book b5 = new Book("C++物件導向","高橋麻奈","978-986-201-699-2",2020,7);
 		Book b6 = new Book("English Now! Listening and Speaking in Everyday Life 3","高橋麻奈","978-626-300-055-1",2020,7);
-		Book b7 = new Book("資訊管理系統","洪錦魁","978-986-7696-39-7",2021,10);
+		Book b7 = new Book("資訊管理系統","董和昇","978-986-7696-39-7",2022,2);
+		Book b8 = new Book("Java最強入門邁向頂尖高手之路:王者歸來","洪錦魁","978-986-5501-58-7",2021,10);
 	}
 
 }
