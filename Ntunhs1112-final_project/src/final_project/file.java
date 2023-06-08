69cc880f48d0b22d212259228fd8f2582a75a261
Jimmy0117 <jimmy930117h@gmail.com>
2023-06-09 04:16:31 +0800

    IO code creat(not done)

--- Ntunhs1112-final_project/src/final_project/file.java
+++ Ntunhs1112-final_project/src/final_project/file.java
@@ -1,4 +1,13 @@
 package final_project;
-public class file {
+import java.io.*;
+
+    public class file {
+        public static void main(String[] args) throws IOException {
+            String s = "Input Text";
+            InputStreamReader in = new InputStreamReader(new FileInputStream("input.txt"));//檔案讀取路徑
+            BufferedReader bin = new BufferedReader(in);
+            BufferedWriter bout = new BufferedWriter(new FileWriter("output.txt"));//檔案輸出路徑
+            
+        }
+    }
     
-}
